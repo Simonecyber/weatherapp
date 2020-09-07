c8a67f406c789bfd92d47b6ab76e1f0b57be2efd
simone franceschetti <f.simone89@gmail.com>
2020-09-07 08:10:44 +0200

    corretto errore env

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 
-require('dotenv').config();
+
 
 const api = {
     key: "process.env.API_KEY",
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
 
 const path = require('path');
+const webpack = require('webpack');
+const dotenv = require('dotenv').config( {
+    path: path.join(__dirname, '.env')
+  } );
 
 module.exports ={
     mode:'development',
@@ -7,5 +11,10 @@ module.exports ={
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugins: [
+        new webpack.DefinePlugin( {
+          "process.env": dotenv.parsed
+        } ),
+      ],
 };
